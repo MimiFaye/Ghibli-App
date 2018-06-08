cb33f111337e2e6107ef4367ab3433ad40c63542
Ferial Malki <ferial.malki@epita.fr>
2018-06-08 19:27:24 +0200

    update

---
+++ MyGhibliApp2/app/src/main/java/fr/epita/myghibliapp/FilmsActivity.kt
@@ -0,0 +1,29 @@
+package fr.epita.myghibliapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_films.*
+
+class FilmsActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(clickedView: View?) {
+        if (clickedView != null) {
+            when (clickedView.id) {
+                R.id.back_button -> {
+                    val intent = Intent(this@FilmsActivity, MainActivity::class.java)
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_films)
+        back_button.setOnClickListener(this@FilmsActivity)
+    }
+
+
+}
--- MyGhibliApp2/app/src/main/java/fr/epita/myghibliapp/MainActivity.kt
+++ MyGhibliApp2/app/src/main/java/fr/epita/myghibliapp/MainActivity.kt
@@ -1,12 +1,27 @@
 package fr.epita.myghibliapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(clickedView: View?) {
+        if (clickedView != null) {
+            when (clickedView.id) {
+                R.id.button_films -> {
+                    val intent = Intent(this@MainActivity, FilmsActivity::class.java)
+
+                R.id.button_characters ->
+                }
+            }
+        }
     }
 }
